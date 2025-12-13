ce0f7a6810337b67fd6b11abe7ec7193f898c935
MKmuslim-777 <muslimuddinkaisanmk@gmail.com>
2025-12-12 22:49:35 -0800

    create a single api for find a single scholarship data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 
@@ -32,6 +32,13 @@ async function run() {
     app.get("/scholarships", async (req, res) => {
       const cursor = scholarshipCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/scholarships/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await scholarshipCollection.findOne(query);
       res.send(result);
     });
 
